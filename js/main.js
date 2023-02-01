890084c56237b4694efd6841533ebfce8a26eb4b
Iara Medina <iaramarielmedina@gmail.com>
2023-01-31 21:40:11 -0300

    Ultima escritura en el repositorio

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ function myFunction(){
     alert('La imagen de perfil no se ha podido cargar. Intente actualizar.');
 }
 
+document.getElementById('botonDescargar').addEventListener('click',function(){
+    document.getElementById('botonDescargar').innerHTML = "Descargado!";
+});
+
+document.getElementById('botonEnviar').addEventListener('click',function(){
+    document.getElementById('botonEnviar').innerHTML = "Enviado!";
+});
 
 
 
